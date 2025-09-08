4063f4e20465a175e4b125b7a5eff0784e759abd
GJoyceArpana <arpanajoyce@gmail.com>
2025-09-08 23:29:45 +0530

    loading page

---
+++ crop_guardian/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'pages/welcome_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CropGuardians',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const WelcomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
